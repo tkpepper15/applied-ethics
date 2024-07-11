e8f67ca6e1b7bee7b898e12e38823dc914eb4a9f
TKpepper15 <tejjas15@gmail.com>
2024-07-11 13:46:52 -0400

    Commit message

--- app/timeline.tsx
+++ app/timeline.tsx
@@ -12,27 +12,30 @@ const TimelineItem: React.FC<TimelineItemProps> = ({ id, title, content, imageUr
 
   return (
     <div className="timeline-item bg-gray-200 p-4 rounded-lg shadow-md">
-      <div className="timeline-content flex flex-col-reverse sm:flex-row-reverse items-center sm:items-start">
-        {imageUrl && (
-          <div className="img-container sm:ml-4 mb-4 sm:mb-0">
-            <img src={imageUrl} alt={title} className="max-w-full h-auto sm:max-w-md lg:max-w-lg" />
-          </div>
-        )}
+      <div className="timeline-content flex flex-col items-center">
         <div className="text-content">
           <h2 className="text-lg font-semibold mb-2">{title}</h2>
           {contentParts.map((part, index) => {
-            const [heading = '', text = ''] = part.split(':', 2);
-            if (heading.trim()) {
-              return (
-                <div key={index} className="mb-1">
-                  <span className="text-md font-semibold">{heading.trim()}:</span>
-                  <span className="text-sm"> {text.trim()}</span>
-                </div>
-              );
-            } else {
-              return <p key={index} className="text-sm mb-1">{part}</p>;
-            }
+            const [heading, ...textParts] = part.split(':');
+            const text = textParts.join(':').trim();
+            return (
+              <div key={index} className="mb-1">
+                {heading?.trim() ? (
+                  <>
+                    <span className="text-md font-semibold">{heading.trim()}:</span>
+                    <span className="text-sm"> {text}</span>
+                  </>
+                ) : (
+                  <p className="text-sm mb-1">{part}</p>
+                )}
+              </div>
+            );
           })}
+                  {imageUrl && (
+          <div className="img-container">
+            <img src={imageUrl} alt={title || 'Timeline Image'} className="max-w-full h-auto" />
+          </div>
+        )}
         </div>
       </div>
     </div>
